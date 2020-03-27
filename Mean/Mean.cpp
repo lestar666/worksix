c5e20685262912f3f46d6e312f3be92583b3ddc5
2077946749 <2077946749@qq.com>
2020-03-27 14:29:25 +0800

    2

---
+++ Mean/Mean.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	VideoCapture cap(0);
+	while (1) {
+		Mat frame;
+		cap >> frame;
+		Mat dstMat;
+		blur(frame, dstMat, Size(3,3));
+		imshow("frame", frame);
+		imshow("dstMat", dstMat);
+		waitKey(30);
+	}
+}
