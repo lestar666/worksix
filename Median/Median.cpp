1b7e33958fcc82761812e1a8ab294317c4c02175
2077946749 <2077946749@qq.com>
2020-03-27 14:24:59 +0800

    1

---
+++ Median/Median.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	VideoCapture cap(0);
+	while (1) {
+		Mat frame;
+		cap >> frame;
+		Mat dstMat;
+		medianBlur(frame, dstMat, 3);
+		imshow("frame", frame);
+		imshow("dstMat", dstMat);
+		waitKey(30);
+	}
+}
